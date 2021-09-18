da4a34b8f14f36ba57b0064f6a5714729a130ed8
Limsunghak <doch2202@naver.com>
2021-09-18 16:11:45 +0900

    394_210917

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView, CreateView
 
 from blog.models import Post, Category, Tag
@@ -57,7 +58,14 @@ class PostDetail(DetailView):
         return context
 
 
-class PostCreateView(CreateView):
+class PostCreate(LoginRequiredMixin, CreateView):
     model = Post
     fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category']
 
+    def form_valid(self, form):
+        current_user = self.request.user
+        if current_user.is_authenticated:
+            form.instance.author = current_user
+            return super(PostCreate, self).form_valid(form)
+        else:
+            return redirect('/blog/')
